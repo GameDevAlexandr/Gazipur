9cc21195076519cd58590a48b9134cc15dd50005
GameDevAlexandr <sp0oky@mail.ru>
2026-04-23 21:25:41 +1000

    Правки

--- Assets/Scripts/System/GameInstaller.cs
+++ Assets/Scripts/System/GameInstaller.cs
@@ -10,6 +10,7 @@ public class GameInstaller : MonoInstaller
     [SerializeField] private GameModeManager _gameModeManager;
     [SerializeField] private CraftManager _craftManager;
     [SerializeField] private ItemsManager _itemsManager;
+    [SerializeField] private MarketManager _market;
     public override void InstallBindings()
     {
         Container.Bind<Control>().FromInstance(_controll).AsSingle();
@@ -18,5 +19,6 @@ public class GameInstaller : MonoInstaller
         Container.Bind<CraftManager>().FromInstance(_craftManager).AsSingle();
         Container.Bind<ItemsManager>().FromInstance(_itemsManager).AsSingle();
         Container.Bind<DataManager>().FromInstance(_data).AsSingle();
+        Container.Bind<MarketManager>().FromInstance(_market).AsSingle();
     }
 }
