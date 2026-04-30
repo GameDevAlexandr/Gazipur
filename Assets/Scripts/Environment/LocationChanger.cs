4ea5773870c80d408864db71543df3fedfdb4d77
brrrzil <a.ne@mail.ru>
2026-04-30 15:21:56 +0300

    Fog

---
+++ Assets/Scripts/Environment/LocationChanger.cs
@@ -0,0 +1,35 @@
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class LocationChanger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AreaVillage"))
+        {
+
+        }
+        
+        if (other.gameObject.CompareTag("AreaRich"))
+        {
+
+        }
+
+        if (other.gameObject.CompareTag("AreaDanger"))
+        {
+
+        }
+    }
+}
