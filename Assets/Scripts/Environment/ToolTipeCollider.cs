39a752e6ce8e617deba4915a1a3885a5057e269c
AndreyN <a.ne@mail.ru>
2026-05-14 20:02:00 +0300

    земля, туман

--- Assets/Scripts/Environment/ToolTipeCollider.cs
+++ Assets/Scripts/Environment/ToolTipeCollider.cs
@@ -5,14 +5,15 @@ public class ToolTipeCollider : MonoBehaviour
 {
     [SerializeField] private string _message;
     [Inject] Tooltipe _tooltipe;
+
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.GetComponent<PlayerMovement>())
         {
             _tooltipe.Show(_message);
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
 
